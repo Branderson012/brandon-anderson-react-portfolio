af029c984cccd3972e0a08e92931f49039615fb1
Brandon Anderson <you@example.commusic_fourlife@yahoo.com>
2019-12-10 19:42:06 -0800

    Building About Page Styles for React Portfolio

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,5 +1,27 @@
 import React from 'react';
+import profilePicture from "../../../static/assets/images/bio/download.jpg";
 
 export default function() {
-  return <div>About</div>;
+  return (
+    <div className="content-page-wrapper">
+      <div 
+        className="left-column" 
+        style={{
+          background: "url(" + profilePicture + ") no-repeat",
+          backgoundSize: "cover",
+          backgroundPosition: "center"
+        }}
+      />
+      <div className="right-column">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Perspiciatis mollitia quibusdam recusandae quas dolor quidem nobis?
+          Voluptate vitae voluptas consequuntur repudiandae nostrum ipsam, ut, soluta quisquam modi facere illum harum est debitis unde! Neque, 
+          Necessitatibus, voluptates unde nisi tempore ad nihil ipsum illo consequuntur laudantium odit sint iste ex et expedita placeat enim ullam impe
+          Consequuntur quis ab eius earum, blanditiis, aperiam, dolorem error dolor commodi odio  Vitae, culpa! Ratione ipsa pariatur quibusdam, inventore cor.
+          Laudantium vitae consequuntur voluptatibus a cumque! Laboriosam dolores possimus deserunt? Ad repellendus deleniti necessitatibus ab, i
+          Possimus explicabo optio earum, dignissimos accusantium voluptates? Dolorem voluptates, quidem pers f, incidunt!
+          Facilis sequi, quam incidunt autem expedita veniam quidem necessitatibus cumqto sequi doloribus doloremque. Dolorum deleniti maiores fuga!
+      </div>
+    </div>
+  );
 }
